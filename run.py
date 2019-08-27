d65c20b0a8782a127d844b89a66fb99963db5e08
SunPaz <matth.amoros@gmail.com>
2019-08-27 14:59:25 -0700

    Basic RabbitMQ reading (from example)

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 """
 TODO :
 
-Start an RPC / Interface
-Authenticate
-Handle message
+Start a RabbitMQ Client
+Get message, behave, answer
 """
+#!/usr/bin/env python
+import pika
+
+queue_name = 'shard'
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue=queue_name)
+
+def callback(ch, method, properties, body):
+	print(" [x] Received %r" % body)
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+print(" [*] Waiting for messages from queue '" + queue_name + "'. To exit press CTRL+C ")
+channel.start_consuming()
